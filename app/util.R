63d5af567a094678ac0f18d48e771ca26363dc8e
guilhermedom <guilhermedfs@gmail.com>
2022-08-06 20:50:46 -0300

    Separate fuzzy system creation to util.R file

---
+++ app/util.R
@@ -0,0 +1,114 @@
+library(sets)
+
+sets_options("universe", seq(0, 100, 1))
+
+util = new.env()
+
+setVariables = function() {
+    variables = set(
+        mathAffinity = fuzzy_partition(
+          varnames = c(mathMin = 5, mathMinor = 15, mathMedian = 50,
+                       mathMajor = 75, mathMax = 90), sd = 10
+        ),
+        interpersonalAffinity = fuzzy_partition(
+          varnames = c(interMin = 30, interMinor = 35, interMedian = 55,
+                       interMajor = 75, interMax = 85), sd = 10
+        ),
+        codeAffinity = fuzzy_partition(
+          varnames = c(codeMin = 10, codeMinor = 25, codeMedian = 50,
+                       codeMajor = 75, codeMax = 95), sd = 10
+        ),
+        leadAffinity = fuzzy_partition(
+          varnames = c(leadMin = 30, leadMinor = 50, leadMedian = 70,
+                       leadMajor = 90, leadMax = 95), sd = 10
+        ),
+        studyAffinity = fuzzy_partition(
+          varnames = c(studyMin = 20, studyMinor = 40, studyMedian = 60,
+                       studyMajor = 80, studyMax = 90), sd = 10
+        ),
+        commAffinity = fuzzy_partition(
+          varnames = c(commMin = 40, commMinor = 50,
+                       commMedian = 60, commMajor = 70,
+                       commMax = 75), sd = 10
+        ),
+        class = fuzzy_partition(
+          varnames = c(poor = 10, average = 50, good = 75,
+                       excellent = 95), sd = 10
+        )
+    )
+    
+    variables
+}
+
+setRules = function() {
+    rules = set(
+        fuzzy_rule(mathAffinity %is% mathMax && interpersonalAffinity %is% interMin
+                   && codeAffinity %is% codeMax && leadAffinity %is% leadMin
+                   && studyAffinity %is% studyMax
+                   && commAffinity %is% commMin, class %is% excellent
+        ),
+        fuzzy_rule(mathAffinity %is% mathMax && interpersonalAffinity %is% interMinor
+                   && codeAffinity %is% codeMajor && leadAffinity %is% leadMin
+                   && studyAffinity %is% studyMax
+                   && commAffinity %is% commMinor, class %is% excellent
+        ),
+        fuzzy_rule(mathAffinity %is% mathMajor && interpersonalAffinity %is% interMinor
+                   && codeAffinity %is% codeMajor && leadAffinity %is% leadMin
+                   && studyAffinity %is% studyMax
+                   && commAffinity %is% commMedian, class %is% excellent
+        ),
+        fuzzy_rule(mathAffinity %is% mathMajor && interpersonalAffinity %is% interMinor
+                   && codeAffinity %is% codeMajor && leadAffinity %is% leadMinor
+                   && studyAffinity %is% studyMajor
+                   && commAffinity %is% commMedian, class %is% good
+        ),
+        fuzzy_rule(mathAffinity %is% mathMajor && interpersonalAffinity %is% interMedian
+                   && codeAffinity %is% codeMedian && leadAffinity %is% leadMedian
+                   && studyAffinity %is% studyMajor
+                   && commAffinity %is% commMajor, class %is% good
+        ),
+        fuzzy_rule(mathAffinity %is% mathMajor && interpersonalAffinity %is% interMedian
+                   && codeAffinity %is% codeMedian && leadAffinity %is% leadMajor
+                   && studyAffinity %is% studyMajor
+                   && commAffinity %is% commMajor, class %is% good
+        ),
+        fuzzy_rule(mathAffinity %is% mathMedian && interpersonalAffinity %is% interMajor
+                   && codeAffinity %is% codeMinor && leadAffinity %is% leadMajor
+                   && studyAffinity %is% studyMedian
+                   && commAffinity %is% commMajor, class %is% average
+        ),
+        fuzzy_rule(mathAffinity %is% mathMedian && interpersonalAffinity %is% interMajor
+                   && codeAffinity %is% codeMinor && leadAffinity %is% leadMajor
+                   && studyAffinity %is% studyMedian
+                   && commAffinity %is% commMax, class %is% average
+        ),
+        fuzzy_rule(mathAffinity %is% mathMinor && interpersonalAffinity %is% interMax
+                   && codeAffinity %is% codeMin && leadAffinity %is% leadMax
+                   && studyAffinity %is% studyMedian
+                   && commAffinity %is% commMax, class %is% average
+        ),
+        fuzzy_rule(mathAffinity %is% mathMinor && interpersonalAffinity %is% interMax
+                   && codeAffinity %is% codeMin && leadAffinity %is% leadMax
+                   && studyAffinity %is% studyMinor
+                   && commAffinity %is% commMax, class %is% poor
+        ),
+        fuzzy_rule(mathAffinity %is% mathMin && interpersonalAffinity %is% interMax
+                   && codeAffinity %is% codeMin && leadAffinity %is% leadMax
+                   && studyAffinity %is% studyMinor
+                   && commAffinity %is% commMax, class %is% poor
+        ),
+        fuzzy_rule(mathAffinity %is% mathMin && interpersonalAffinity %is% interMax
+                   && codeAffinity %is% codeMin && leadAffinity %is% leadMax
+                   && studyAffinity %is% studyMin
+                   && commAffinity %is% commMax, class %is% poor
+        )
+    )
+    
+    rules
+}
+
+util$setFuzzySystem = function() {
+    system = fuzzy_system(setVariables(), setRules())
+    
+    system
+}
